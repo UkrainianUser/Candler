5c979790647101949af15b22fc718231d838a7f6
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-09-24 01:58:22 +0300

    add accordeon

--- js/main.js
+++ js/main.js
@@ -28,3 +28,21 @@ const swiperTwo = new Swiper(".quality__slider", {
 		},
 	},
 });
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = accordeon.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (title) {
+	title.addEventListener("click", function () {
+		const currentText = title.parentElement.querySelector(".accordeon__text");
+
+		title.classList.toggle("accordeon__title--active");
+		currentText.classList.toggle("accordeon__text--visible");
+
+		if (currentText.classList.contains("accordeon__text--visible")) {
+			currentText.style.maxHeight = currentText.scrollHeight + "px";
+		} else {
+			currentText.style.maxHeight = null;
+		}
+	});
+});
